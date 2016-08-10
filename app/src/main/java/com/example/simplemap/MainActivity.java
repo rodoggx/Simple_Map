b2726b39ca00f4d5c2a17fcdf102b2ae49904c91
rodoggx <mikerodolph@yahoo.com>
2016-08-09 20:45:58 -0400

    add google map

---
+++ app/src/main/java/com/example/simplemap/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.simplemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.a_main_maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Log.d(TAG, "onMapReady: ");
+        LatLng madagascar = new LatLng(-19, 47);
+        googleMap.addMarker(new MarkerOptions().position(madagascar).title("Where is Marty"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(madagascar));
+    }
+}
